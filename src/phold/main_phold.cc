99a4b640874f0ccac6ef4c4bbeddb67edd3510b9
Masa Hanai <masa.hanai@gmail.com>
2016-08-04 17:00:29 +0800

    TODO read parameter from commandline

--- src/phold/main_phold.cc
+++ src/phold/main_phold.cc
@@ -164,6 +164,11 @@ int main(int argc, char* argv[]) {
   NUM_INIT_MSG = std::stol(argv[2]);
   REMOTE_COM_RATIO = std::stod(argv[3]);
   LAMBDA = std::stod(argv[4]);
+  // TODO
+  NUM_WHAT_IF = 1;
+  GSYNC_INTERVAL = 10;
+  LP_INTERVAL = 4;
+  GTW_CUT_INTERVAL = 50;
 
   scalesim::runner<phold>::run();
   return 0;
